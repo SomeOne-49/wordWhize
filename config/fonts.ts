d21251f4d234d02582496e8eaad8d609c4b74bd4
Albaraa Alismaeel <albraa@vega4it.com>
2024-08-15 14:18:12 +0300

    init project

---
+++ config/fonts.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line camelcase
+import { Baloo_Bhaijaan_2 } from 'next/font/google';
+
+export const baloo = Baloo_Bhaijaan_2({
+  subsets: ['latin', 'arabic'],
+  weight: ['400', '500', '600', '700'],
+});
---
+++ config/site.ts
@@ -0,0 +1,20 @@
+
+export const siteConfig = {
+	name: "Word Whiz",
+	description: "Make beautiful websites regardless of your design experience.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+	],
+	links: {
+		github: "https://github.com/nextui-org/nextui",
+		twitter: "https://twitter.com/getnextui",
+		docs: "https://nextui.org",
+		discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev"
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
